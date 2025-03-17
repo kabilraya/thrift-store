382a89a83f2f15b9349e04b428ed6c54720a9b9d
Kabil Raya <roykabil99@gmail.com>
2025-03-17 20:01:47 +0545

    Initial Commit

--- frontend/my-app/src/components/Register.js
+++ frontend/my-app/src/components/Register.js
@@ -8,28 +8,12 @@ const Register = () => {
         <h2>Register</h2>
         <form>
           <div className="form-group">
-            <label>First Name</label>
-            <input type="text" placeholder="Enter your first name" required />
-          </div>
-          <div className="form-group">
-            <label>Last Name</label>
-            <input type="text" placeholder="Enter your last name" required />
-          </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email" required />
+            <label>Username</label>
+            <input type="text" placeholder="Enter your username" required />
           </div>
           <div className="form-group">
             <label>Password</label>
             <input type="password" placeholder="Enter your password" required />
-          </div>
-          <div className="form-group">
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm your password"
-              required
-            />
           </div>
           <button type="submit" className="auth-button">
             Register
